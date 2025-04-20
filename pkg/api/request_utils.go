2291a6d5ff86a167920e477f3a08691d34547247
CC11001100 <CC11001100@qq.com>
2025-04-20 17:48:30 +0800

    docs: 完善请求工具函数的注释说明

--- pkg/api/request_utils.go
+++ pkg/api/request_utils.go
@@ -17,44 +17,39 @@ import (
 // 返回:
 //   - error: 如果保存失败，返回错误
 func SaveRequest(req *Request, store storage.Storage) error {
+	// 解析时间戳
+	var timestamp time.Time
+	if req.Timestamp != "" {
+		var err error
+		timestamp, err = time.Parse(time.RFC3339, req.Timestamp)
+		if err != nil {
+			// 如果解析失败，使用当前时间
+			timestamp = time.Now()
+		}
+	} else {
+		timestamp = time.Now()
+	}
+
 	// 转换API请求模型为存储请求模型
 	storageReq := &storage.Request{
 		ID:        req.ID,
 		Method:    req.Method,
 		Path:      req.Path,
-		Timestamp: req.Timestamp,
+		Timestamp: timestamp,
 		ClientIP:  req.IPAddress,
+		IPAddress: req.IPAddress,
+		Headers:   req.Headers,
+		Query:     req.Query,
+		Body:      req.Body,
 	}
-
-	// 转换Headers（从map[string]string到map[string][]string）
-	headers := make(map[string][]string)
-	for k, v := range req.Headers {
-		headers[k] = []string{v}
-	}
-	storageReq.Headers = headers
-
-	// 转换Query（从map[string]string到map[string][]string）
-	query := make(map[string][]string)
-	for k, v := range req.Query {
-		query[k] = []string{v}
-	}
-	storageReq.Query = query
-
-	// 设置请求体
-	storageReq.Body = req.Body
 
 	// 如果有响应，也转换响应
 	if req.Response != nil {
-		// 转换响应头
-		respHeaders := make(map[string][]string)
-		for k, v := range req.Response.Headers {
-			respHeaders[k] = []string{v}
-		}
-
 		storageReq.Response = &storage.ProxyResponse{
 			StatusCode: req.Response.StatusCode,
-			Headers:    respHeaders,
+			Headers:    req.Response.Headers,
 			Body:       req.Response.Body,
+			Latency:    req.Response.Latency,
 		}
 	}
 
@@ -78,15 +73,7 @@ func ConvertStorageToAPIRequest(req *storage.Request) *Request {
 	}
 
 	// 处理时间戳
-	switch ts := req.Timestamp.(type) {
-	case string:
-		apiReq.Timestamp = ts
-	case time.Time:
-		apiReq.Timestamp = ts.Format(time.RFC3339)
-	default:
-		// 如果无法识别时间戳类型，使用当前时间
-		apiReq.Timestamp = time.Now().Format(time.RFC3339)
-	}
+	apiReq.Timestamp = req.Timestamp.Format(time.RFC3339)
 
 	// 处理IP地址（兼容两个字段）
 	if req.ClientIP != "" {
@@ -95,52 +82,19 @@ func ConvertStorageToAPIRequest(req *storage.Request) *Request {
 		apiReq.IPAddress = req.IPAddress
 	}
 
-	// 转换Headers
-	apiReq.Headers = make(map[string]string)
-	switch headers := req.Headers.(type) {
-	case map[string][]string:
-		for k, v := range headers {
-			if len(v) > 0 {
-				apiReq.Headers[k] = v[0]
-			}
-		}
-	case map[string]string:
-		apiReq.Headers = headers
-	}
+	// 转换Headers - 现在Headers已经是map[string]string
+	apiReq.Headers = req.Headers
 
-	// 转换Query
-	apiReq.Query = make(map[string]string)
-	switch query := req.Query.(type) {
-	case map[string][]string:
-		for k, v := range query {
-			if len(v) > 0 {
-				apiReq.Query[k] = v[0]
-			}
-		}
-	case map[string]string:
-		apiReq.Query = query
-	}
+	// 转换Query - 现在Query已经是map[string]string
+	apiReq.Query = req.Query
 
 	// 转换Response（如果存在）
 	if req.Response != nil {
-		respHeaders := make(map[string]string)
-
-		// 转换响应头
-		switch headers := req.Response.Headers.(type) {
-		case map[string][]string:
-			for k, v := range headers {
-				if len(v) > 0 {
-					respHeaders[k] = v[0]
-				}
-			}
-		case map[string]string:
-			respHeaders = headers
-		}
-
 		apiReq.Response = &ProxyResponse{
 			StatusCode: req.Response.StatusCode,
-			Headers:    respHeaders,
+			Headers:    req.Response.Headers,
 			Body:       req.Response.Body,
+			Latency:    req.Response.Latency,
 		}
 	}
 
